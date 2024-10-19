8f95fb9c5bc23972177da02bee6f5154a34eab3b
m-machala <114995208+m-machala@users.noreply.github.com>
2024-10-19 08:58:14 +0200

    add export functions list and explanation

--- src/packages/base_classes/Renderer.py
+++ src/packages/base_classes/Renderer.py
@@ -2,9 +2,13 @@ from abc import ABC, abstractmethod
 
 # The renderer class is used to display an image of the current running environment.
 # You need to implement the render abstract method in your renderer.
+# You can also put function references into the exportFunctions list in the form of a tuple.
+# The tuple has to contain a name and a function reference.
+# An example of a correct tuple would be: ("Zoom in", zoomImage)  where zoomImage is a function in your code.
 class Renderer(ABC):
     def __init__(self, outputResolutionW, outputResolutionH):
         self.setOutputResolution(outputResolutionW, outputResolutionH)
+        self.exportFunctions = []
 
     # The render function should return an image with the output resolution 
     # set in the constructor/setOutputResolution function.
@@ -15,3 +19,6 @@ class Renderer(ABC):
     def setOutputResolution(self, outputResolutionW, outputResolutionH):
         self.outputResolutionW = outputResolutionW
         self.outputResolutionH = outputResolutionH
+
+    def getExportFunctions(self):
+        return self.exportFunctions
