df381cc3b7fd0a44277fb9fe13c679ef7c56ce96
m-machala <114995208+m-machala@users.noreply.github.com>
2025-01-26 11:33:46 +0100

    create initial layour for the welcome screen

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -1,9 +1,61 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QListWidget, QTextEdit
 
 class WelcomeScreen(QWidget):
     def __init__(self):
         super().__init__()
 
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("test"))
-        self.setLayout(layout)
+        mainLayout = QVBoxLayout()
+
+        welcomeLabel = QLabel("Welcome to OCL!")
+        mainLayout.addWidget(welcomeLabel)
+
+        infoLabel = QLabel("To begin, please select an environment from the list below.")
+        mainLayout.addWidget(infoLabel)
+
+        selectionLayout = QHBoxLayout()
+        mainLayout.addLayout(selectionLayout)
+
+
+        moduleListLayout = QVBoxLayout()
+        selectionLayout.addLayout(moduleListLayout)
+
+        moduleLabel = QLabel("Available modules:")
+        moduleListLayout.addWidget(moduleLabel)
+
+        moduleList = QListWidget()
+        moduleListLayout.addWidget(moduleList)
+
+
+        moduleInfoLayout = QVBoxLayout()
+        selectionLayout.addLayout(moduleInfoLayout)
+
+        cellLabel = QLabel("Available cell packs:")
+        moduleInfoLayout.addWidget(cellLabel)
+
+        cellList = QListWidget()
+        moduleInfoLayout.addWidget(cellList)
+
+        moduleLabel = QLabel("Selected module info:")
+        moduleInfoLayout.addWidget(moduleLabel)
+
+        moduleInfo = QTextEdit()
+        moduleInfo.setReadOnly(True)
+        moduleInfoLayout.addWidget(moduleInfo)
+
+
+        reloadButton = QPushButton("Reload")
+        mainLayout.addWidget(reloadButton)
+
+
+        buttonLayout = QHBoxLayout()
+        mainLayout.addLayout(buttonLayout)
+
+        exitButton = QPushButton("Exit")
+        buttonLayout.addWidget(exitButton)
+
+        buttonLayout.addStretch()
+
+        beginButton = QPushButton("Begin")
+        buttonLayout.addWidget(beginButton)
+
+        self.setLayout(mainLayout)
