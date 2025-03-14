68be2470ed354c0c02c203fb995a710662c15dd2
m-machala <114995208+m-machala@users.noreply.github.com>
2025-03-14 16:41:17 +0100

    set app theme to fusion

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from PyQt6.QtWidgets import QApplication
 import sys
 
 app = QApplication(sys.argv)
+app.setStyle("Fusion")
 main_window = WelcomeScreen()
 
 main_window.setWindowTitle("Open Cell Lab")
