4a003a27e3fe4c8a64fe0204f8873fa659956214
m-machala <114995208+m-machala@users.noreply.github.com>
2025-03-15 15:58:42 +0100

    simplify wall cell code

--- src/packages/Simple2D/environments/Simple2DEnvironment/cellPacks/Walls.py
+++ src/packages/Simple2D/environments/Simple2DEnvironment/cellPacks/Walls.py
@@ -3,17 +3,11 @@ from base_classes.CellBrain import CellBrain
 class WhiteWall(CellBrain):
     COLOR = (255, 255, 255)
 
-    def __init__(self, environment):
-        super().__init__(environment)
-
     def run(self):
         pass
         
 class GrayWall(CellBrain):
     COLOR = (127, 127, 127)
-
-    def __init__(self, environment):
-        super().__init__(environment)
 
     def run(self):
         pass
