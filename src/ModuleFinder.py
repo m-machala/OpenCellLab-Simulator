92507dc7e6dd2191c28d12642b60434ef2110d46
m-machala <114995208+m-machala@users.noreply.github.com>
2026-03-30 15:05:03 +0200

    add more comments to loader base class, fix loader data metadata, add initial implementation of loader (data) module validation/loading

--- src/ModuleFinder.py
+++ src/ModuleFinder.py
@@ -11,18 +11,22 @@ def findAllJSONs(folderPath):
                 foundJSONs.append(os.path.join(root, file))
     return foundJSONs
 
-validPackages = ["renderer", "environment", "cell"]
+validPackages = ["renderer", "environment", "cell", "loader", "loader data"]
 def validatePackageJSON(JSON):
     output = False
     if "package type" in JSON and JSON["package type"] in validPackages and "package name" in JSON and JSON["package name"] != "" and "package path" in JSON and JSON["package path"] != "":
-        if JSON["package type"] == "cell" and "cell types" in JSON:
+        if JSON["package type"] == "cell":
             output = True
-            if len(JSON["cell types"]) <= 0:
+            if not "cell types" in JSON or len(JSON["cell types"]) <= 0:
                 output = False
             else:
                 for cellType in JSON["cell types"]:
                     if not ("cell name" in cellType and "cell class" in cellType and cellType["cell name"] != "" and cellType["cell class"] != ""):
                         output = False
+        elif JSON["package type"] == "loader data":
+            output = True
+            if len(JSON["cell types"]) <= 0 or ("environment type"  in JSON and JSON["environment type"] == "any"):
+                output = False
         else:
             if "package class" in JSON and JSON["package class"] != "":
                 output = True
@@ -63,6 +67,8 @@ def removeJSONsWithoutDependencies(JSONList):
     renderers = filterJSONsByType(JSONList, "renderer")
     environments = filterJSONsByType(JSONList, "environment")
     cellPacks = filterJSONsByType(JSONList, "cell")
+    loaders = filterJSONsByType(JSONList, "loader")
+    loaderData = filterJSONsByType(JSONList, "loader data")
 
     rendererClassNames = []
     for renderer in renderers:
@@ -71,18 +77,44 @@ def removeJSONsWithoutDependencies(JSONList):
     validEnvironments = []
     environmentClassNames = []
     for environment in environments:
-        if "renderer class" in environment and environment["renderer class"] != "":
-            if environment["renderer class"] in rendererClassNames:
-                environmentClassNames.append(environment["package class"])
-                validEnvironments.append(environment)
+        if "renderer class" in environment and environment["renderer class"] != "" and environment["renderer class"] in rendererClassNames:
+            environmentClassNames.append(environment["package class"])
+            validEnvironments.append(environment)
                     
     validCellPacks = []
+    cellClassNames = []
     for cellPack in cellPacks:
-        if "environment class" in cellPack and cellPack["environment class"] != "":
-            if cellPack["environment class"] in environmentClassNames:
+        if "environment class" in cellPack and cellPack["environment class"] != "" and cellPack["environment class"] in environmentClassNames:
+            empty = True
+            for cellType in cellPack["cell types"]:
+                empty = False
+                cellClassNames.append(cellType["cell class"])
+            if not empty:
                 validCellPacks.append(cellPack)
 
-    return (renderers, validEnvironments, validCellPacks)
+    validLoaders = []
+    loaderClassNames = []
+    for loader in loaders:
+        if "package class" in loader and loader["package class"] != "":
+            validLoaders.append(loader)
+            loaderClassNames.append(loader["package class"])
+
+    validLoaderData = []
+    for loaderDatum in loaderData:
+        if "environment class" in loaderDatum and loaderDatum["environment class"] != "" and (loaderDatum["environment class"] in environmentClassNames or loaderDatum["environment class"] == "any") and "loader class" in loaderDatum and loaderDatum["loader class"] in loaderClassNames:
+            empty = True
+            complete = True
+            for cellClass in loaderDatum["cell classes"]:
+                if cellClass in cellClassNames:
+                    empty = False
+                else:
+                    complete = False
+            
+            if not empty:
+                loaderDatum["complete"] = complete
+                validLoaderData.append(loaderDatum)
+
+    return (renderers, validEnvironments, validCellPacks, validLoaders, validLoaderData)
 
 def loadRenderer(rendererJSON):
     moduleSpec = importlib.util.spec_from_file_location(rendererJSON["package name"], rendererJSON["package path"])
@@ -118,4 +150,7 @@ def loadCellPack(cellPackJSON):
     except Exception as e:
         pass        
     return cells
+
+def loadLoader(loaderJSON):
+    return loadRenderer(loaderJSON)
     
--- src/base_classes/Loader.py
+++ src/base_classes/Loader.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
 # module for loading cells into the simulator without having to place them manually
+# loaders can, but don't have to, be made for a specific environment. if it is made for a specific envirnoment, it should create cells through this environment
+# otherwise it should create cells through the cell executor
 class Loader(ABC):
     # returns a list of cells it wants to load in the desired execution order
     @abstractmethod
